3e9d3f40436648cdbcfa4f3b491c2fb1c8b1c7c0
61011212075 <61011212075@msu.ac.th>
2020-12-23 23:37:32 +0700

    update login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,23 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import {ToolbarModule} from 'primeng/toolbar';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ToolbarModule
+    ToolbarModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
